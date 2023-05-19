b96597d31596bb7c3d971b9b9c8149715ceb4634
Erica Nshimirimana <e.nshimirim@alustudent.com>
2023-05-19 12:14:16 +0300

    base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+import models
+
+class BaseModel:
+
+    """Initialise class"""
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    pass
+                elif key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
+
+    """modify the updated_at attribute"""
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    """return string representation"""
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    """return dict"""
+
+    def to_dict(self):
+        obj = dict(self.__dict__)
+        obj["__class__"] = self.__class__.__name__
+        obj["created_at"] = obj["created_at"].isoformat()
+        obj["updated_at"] = obj["updated_at"].isoformat()
+        return obj
